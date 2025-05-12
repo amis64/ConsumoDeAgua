40554cb142d6b254e30117a06208439fa6c12974
Amy Gelves <amy.gelves@hotmail.com>
2025-05-12 16:50:33 -0500

    preparacion para publicar en render

--- app.py
+++ app.py
@@ -415,5 +415,5 @@ if __name__ == '__main__':
     # Inicializar servicio del modelo al arrancar
     init_model_service()
     
-    # Ejecutar aplicación Flask
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
